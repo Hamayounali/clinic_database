cb74a567aa775393038e9ef1344c12b6f0e09870
Alfred Boateng <programmingwithalfred@gmail.com>
2022-09-27 10:31:30 +0000

    Update scheme

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,41 +1,43 @@
+CREATE TABLE patients (
+    id  SERIAL PRIMARY KEY,
+    name varchar(100),
+    date_of_birth date
+);
+
 CREATE TABLE medical_histories (
-    ID  SERIAL PRIMARY KEY,
+    id  SERIAL PRIMARY KEY,
     admitted_at timestamp,
-    patient_it int,
+    patient_id int,
     status varchar(100)
 );
 
-CREATE TABLE patients (
-    ID  SERIAL PRIMARY KEY,
-    name varchar(100),
-    date_of_birth data
-);
-
 CREATE TABLE invoices (
-    ID  SERIAL PRIMARY KEY,
+    id  SERIAL PRIMARY KEY,
     total_amount decimal(10,2),
     generated_at timestamp,
     payed_at timestamp,
     medical_history_id int
 );
 
-CREATE TABLE invoice_items (
-    ID  SERIAL PRIMARY KEY,
-    unit_price decimal(10,2),
-    quantity int,
-    totla_price decimal(10,2),
-    invoice_id int,
-    treatment_it int
-);
-
 CREATE TABLE treatments (
-    ID  SERIAL PRIMARY KEY,
+    id  SERIAL PRIMARY KEY,
     type varchar(100);
     name varchar(100);
 );
 
-create table patient_treatments (
-    ID SERIAL PRIMARY KEY,
+
+CREATE TABLE invoice_items (
+    id  SERIAL PRIMARY KEY,
+    unit_price decimal(10,2),
+    quantity int,
+    total_price decimal(10,2),
+    invoice_id int,
+    treatment_id int
+);
+
+
+create TABLE patient_treatments (
+    id SERIAL PRIMARY KEY,
     patient_id int,
     treatment_id int,
     FOREIGN KEY ( patient_id) REFERENCES patients(id),
@@ -43,7 +45,7 @@ create table patient_treatments (
 );
 
 CREATE TABLE patient_medical_histories (
-    ID SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     patient_id int,
     medical_histories_id INT,
     FOREIGN KEY (medical_histories_id) REFERENCES medical_histories(id),
